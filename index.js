203e9b4a276c6e579e6a45acf67823f31ef657db
Rtroman14 <rtroman14@gmail.com>
2020-07-13 08:31:02 -0600

    first commit

---
+++ index.js
@@ -0,0 +1,14 @@
+const repo = require("./repo");
+
+// CREATE
+// repo.create({ firstName: "Ryan", lastName: "Roman" });
+
+// READ
+// repo.getAll().then((all) => console.log(all));
+
+// repo.getUser("dab45d9a").then((user) => console.log(user));
+
+// repo.getUserBy({ lastName: "Roman" }).then((user) => console.log(user));
+
+// UPDATE
+// repo.update("dab45d9a", { age: 26 }).then((user) => console.log(user));
